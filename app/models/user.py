7b77ad164909e5b0df74d683f7c7b292b69cd441
Joshua <joshua.hulford@gmail.com>
2023-06-02 19:07:51 -0400

    add comments model

--- app/models/user.py
+++ app/models/user.py
@@ -18,6 +18,7 @@ class User(db.Model, UserMixin):
     bio = db.Column(db.Text, nullable=False)
 
     funding = db.relationship('Funding', back_populates='user')
+    comments = db.relationship('Comment', back_populates='user')
 
     @property
     def password(self):
