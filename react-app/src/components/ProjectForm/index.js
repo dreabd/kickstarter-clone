030d408e58eb363f116e46c523f255ec4b382df0
Andre <andreabad17@gmail.com>
2023-06-05 12:50:32 -0700

    Imported Thunk

--- react-app/src/components/ProjectForm/index.js
+++ react-app/src/components/ProjectForm/index.js
@@ -1,8 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
-import postNewProjectThunk from //////////////////////////////////////////////////
-
+import { postNewProjectThunk } from '../../store/projects';
 
 function CreateProjectForm() {
 
