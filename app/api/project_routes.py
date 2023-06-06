093ef98af020eab716d9a0341a649098bbe4b1d5
Andre <andreabad17@gmail.com>
2023-06-05 23:25:03 -0700

    Got the api/projects/current to send the right information back

--- app/api/project_routes.py
+++ app/api/project_routes.py
@@ -8,6 +8,23 @@ from .AWS_helpers import upload_file_to_s3, get_unique_filename, remove_file_fro
 
 project_routes = Blueprint("projects", __name__)
 
+@project_routes.route("/current")
+def get_current_user_project():
+    '''
+    Grabs all the spots owned by the current user
+    '''
+
+    id = current_user.id
+    print("This is the id of the current user.........", id)
+
+    projects = Project.query.filter(Project.user_id == id)
+
+    res = [project.to_dict() for project in projects  ]
+
+    return {"response": res }
+
+
+
 
 @project_routes.route("/<int:id>")
 def get_single_project(id):
@@ -77,6 +94,9 @@ def post_new_project():
     if form.errors:
         print("There were some form errors", form.errors)
         return form.errors, 400
+
+
+
 
 
 @project_routes.route("/")
