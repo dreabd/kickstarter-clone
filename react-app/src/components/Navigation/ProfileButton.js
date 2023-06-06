a45539f185fd466026af59d3632606d37e6d1eee
Joshua <joshua.hulford@gmail.com>
2023-06-06 10:08:34 -0400

    Some styling on the navbar

--- react-app/src/components/Navigation/ProfileButton.js
+++ react-app/src/components/Navigation/ProfileButton.js
@@ -39,7 +39,7 @@ function ProfileButton({ user }) {
 
   return (
     <>
-      <button onClick={openMenu}>
+      <button onClick={openMenu} className="header-dropdown-button">
         <i className="fas fa-user-circle" />
       </button>
       <ul className={ulClassName} ref={ulRef}>
