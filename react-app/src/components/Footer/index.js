476d384e10afed23a39f97a7d6698bbb406d9cf7
Joshua <joshua.hulford@gmail.com>
2023-06-09 16:10:36 -0400

    footer styling and feature image size

--- react-app/src/components/Footer/index.js
+++ react-app/src/components/Footer/index.js
@@ -6,25 +6,33 @@ const Footer = () => {
         <>
             <h2 className='footer-title'>Meet the Developers</h2>
             <div className='footer-container'>
-                <div className='link-container'>
+                <div className='person-container'>
                     <p>Andre Abad</p>
-                    <a href='https://www.linkedin.com/in/andre-chris-abad-b1a55215a/'><i class="fa-brands fa-linkedin"></i></a>
-                    <a href='https://github.com/dreabd'><i class="fa-brands fa-square-github"></i></a>
+                    <div className='link-container'>
+                        <a href='https://www.linkedin.com/in/andre-chris-abad-b1a55215a/'><i class="fa-brands fa-linkedin"></i></a>
+                        <a href='https://github.com/dreabd'><i class="fa-brands fa-square-github"></i></a>
+                    </div>
                 </div>
-                <div className='link-container'>
-                    <p>Joshua Hulford</p>
-                    <a href='https://www.linkedin.com/in/joshua-hulford/'><i class="fa-brands fa-linkedin"></i></a>
-                    <a href='https://github.com/Jhulford88'><i class="fa-brands fa-square-github"></i></a>
+                <div className='person-container'>
+                        <p>Joshua Hulford</p>
+                    <div className='link-container'>
+                        <a href='https://www.linkedin.com/in/joshua-hulford/'><i class="fa-brands fa-linkedin"></i></a>
+                        <a href='https://github.com/Jhulford88'><i class="fa-brands fa-square-github"></i></a>
+                    </div>
                 </div>
-                <div className='link-container'>
-                    <p>Liz Lowry</p>
-                    <a href='https://www.linkedin.com/in/elizabeth-lowry-33201a14'><i class="fa-brands fa-linkedin"></i></a>
-                    <a href='https://github.com/LizLow25'><i class="fa-brands fa-square-github"></i></a>
+                <div className='person-container'>
+                        <p>Liz Lowry</p>
+                    <div className='link-container'>
+                        <a href='https://www.linkedin.com/in/elizabeth-lowry-33201a14'><i class="fa-brands fa-linkedin"></i></a>
+                        <a href='https://github.com/LizLow25'><i class="fa-brands fa-square-github"></i></a>
+                    </div>
                 </div>
-                <div className='link-container'>
-                    <p>Tia Doherty</p>
-                    <a href='https://www.linkedin.com/in/tiadoherty/'><i class="fa-brands fa-linkedin"></i></a>
-                    <a href='https://github.com/tiadoherty'><i class="fa-brands fa-square-github"></i></a>
+                <div className='person-container'>
+                        <p>Tia Doherty</p>
+                    <div className='link-container'>
+                        <a href='https://www.linkedin.com/in/tiadoherty/'><i class="fa-brands fa-linkedin"></i></a>
+                        <a href='https://github.com/tiadoherty'><i class="fa-brands fa-square-github"></i></a>
+                    </div>
                 </div>
             </div>
         </>
